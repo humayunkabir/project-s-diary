2319dbeebfa72aa8a019ee2876dfbd3fb8180878
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-10-11 23:31:44 +0600

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">Diary</div>
+        <div className="App">
+          <Navbar />
+        </div>
       </BrowserRouter>
     );
   }
