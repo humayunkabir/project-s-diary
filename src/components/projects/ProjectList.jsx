8ae9a47b179a857ffcd9976637f766f5bd06c7a6
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-10-12 00:03:09 +0600

    Project Summary Component

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-            esse recusandae laborum corrupti perspiciatis! Hic fugit sequi
-            obcaecati illum et animi numquam quam, consequuntur at ad sed quod
-            asperiores error.
-          </p>
-          <p className="grey-text">3rd September, 2AM</p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ src/components/projects/ProjectSummary.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProjectSummary = props => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit esse
+          recusandae laborum corrupti perspiciatis! Hic fugit sequi obcaecati
+          illum et animi numquam quam, consequuntur at ad sed quod asperiores
+          error.
+        </p>
+        <p className="grey-text">3rd September, 2AM</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
